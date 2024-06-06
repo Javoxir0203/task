f49c5a623e0678f31363ebc3998688001b59e667
Eshquvvatov Javoxir <javoxireshquvvatovv@gmail.com>
2024-06-07 00:26:04 +0500

    card page

--- app/(root)/_components/card.tsx
+++ app/(root)/_components/card.tsx
@@ -10,14 +10,14 @@ const Card = () => {
 			<div className='flex flex-col'>
 				<div className='flex mb-5  gap-8'>
 					<div className='flex p-8  rounded-md border gap-5  w-full'>
-						<Image src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
+						<Image className='rounded-md' src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
 						<div className='flex flex-col'>
 							<h2 className='text-xl font-bold'>UX Driven Engineering</h2>
 							<p className='text-gray-600'>Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.</p>
 						</div>
 					</div>
 					<div className='flex  rounded-md  p-8 border gap-5  w-full'>
-						<Image src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
+						<Image className='rounded-md' src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
 						<div className='flex flex-col'>
 							<h2 className='text-xl font-bold'>UX Driven Engineering</h2>
 							<p className='text-gray-600'>Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.</p>
@@ -27,14 +27,14 @@ const Card = () => {
 
 				<div className='flex mb-5  gap-8'>
 					<div className='flex p-8 rounded-md  border gap-5  w-full'>
-						<Image src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
+						<Image className='rounded-md' src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
 						<div className='flex flex-col'>
 							<h2 className='text-xl font-bold'>UX Driven Engineering</h2>
 							<p className='text-gray-600'>Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.</p>
 						</div>
 					</div>
 					<div className='flex  p-8 rounded-md  border gap-5  w-full'>
-						<Image src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
+						<Image className='rounded-md' src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
 						<div className='flex flex-col'>
 							<h2 className='text-xl font-bold'>UX Driven Engineering</h2>
 							<p className='text-gray-600'>Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.</p>
@@ -44,14 +44,14 @@ const Card = () => {
 
 				<div className='flex  gap-8'>
 					<div className='flex p-8 rounded-md  border gap-5  w-full'>
-						<Image src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
+						<Image className='rounded-md' src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
 						<div className='flex flex-col'>
 							<h2 className='text-xl font-bold'>UX Driven Engineering</h2>
 							<p className='text-gray-600'>Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.</p>
 						</div>
 					</div>
 					<div className='flex  p-8 rounded-md  border gap-5  w-full'>
-						<Image src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
+						<Image className='rounded-md' src={'/card/Rocket_perspective_matte.svg'} width={100} height={100} alt={'logo'} />
 						<div className='flex flex-col'>
 							<h2 className='text-xl font-bold'>UX Driven Engineering</h2>
 							<p className='text-gray-600'>Unlike other companies, we are a UX first development company. Projects are driven by designers and they make sure design and experiences translate to code.</p>
